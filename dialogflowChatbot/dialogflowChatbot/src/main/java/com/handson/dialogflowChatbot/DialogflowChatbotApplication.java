23dd26e8649bb1a12bad6956af928fb843ef11b0
David Desse <daviddesse2@gmail.com>
2024-01-16 18:53:41 +0200

    create

---
+++ dialogflowChatbot/dialogflowChatbot/src/main/java/com/handson/dialogflowChatbot/DialogflowChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.handson.dialogflowChatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DialogflowChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DialogflowChatbotApplication.class, args);
+	}
+
+}
---
+++ dialogflowChatbot/dialogflowChatbot/src/test/java/com/handson/dialogflowChatbot/DialogflowChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handson.dialogflowChatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DialogflowChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
